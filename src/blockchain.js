f8011d77a2edd0dec917ef2c7cd2356ce9c528da
Handole Kim <handolekim@Handoles-MacBook-Pro.local>
2018-10-01 15:10:12 -0400

    src/blockchain.js genesis block created

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,14 @@ class Block {
         this.data = data;
     }
 }
+
+const genesisBlock = new Block(
+    0,
+    "44BEB3DB011604760F5BD2F764F6ADA32E0ABA1D6158F27A2710455857DA9939",
+    null,
+    1520312194926,
+    "This is the genesis!"
+);
+
+let blockchain = [genesisBlock];
+
