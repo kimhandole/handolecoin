e19e073d7ec4907b6827155929ad729bb7c7bd42
Handole Kim <handolekim@Handoles-MacBook-Pro.local>
2018-10-01 15:29:18 -0400

    src/blockchain.js createNewBlock added

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class Block {
     constructor(index, hash, previousHash, timestamp, data){
         this.index = index;
@@ -22,8 +24,26 @@ const getLastBlock = () => blockchain[blockchain.length - 1];
 
 const getTimeStamp = () => new Date().getTime() / 1000;
 
+const createHash = (index, previousHash, timestamp, data) => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+
 const createNewBlcok = data => {
     const previousBlock = getLastBlock();
     const newBlockIndex = previousBlock.index + 1;
     const newTimestamp = getTimeStamp();
+    const newHash = createHash(
+        newBlockIndex, 
+        previousBlock.hash, 
+        newTimestamp, 
+        data
+    );
+    const newBlock = new Block(
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    return newBlock;
 };
