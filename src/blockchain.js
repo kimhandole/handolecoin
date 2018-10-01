e50c49684d5c89a1f450e08561808e96a930cbcb
Handole Kim <handolekim@Handoles-MacBook-Pro.local>
2018-10-01 15:17:09 -0400

    src/blockchain.js working on createNewBlock function

--- src/blockchain.js
+++ src/blockchain.js
@@ -10,11 +10,20 @@ class Block {
 
 const genesisBlock = new Block(
     0,
-    "44BEB3DB011604760F5BD2F764F6ADA32E0ABA1D6158F27A2710455857DA9939",
+    "6AE7F705FD92D09E36A198DCDCE2888FA35696DC72B4BA4388860648DF48B592",
     null,
-    1520312194926,
+    1538421292.09,
     "This is the genesis!"
 );
 
 let blockchain = [genesisBlock];
 
+const getLastBlock = () => blockchain[blockchain.length - 1];
+
+const getTimeStamp = () => new Date().getTime() / 1000;
+
+const createNewBlcok = data => {
+    const previousBlock = getLastBlock();
+    const newBlockIndex = previousBlock.index + 1;
+    const newTimestamp = getTimeStamp();
+};
