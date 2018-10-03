63e7f81d34bad8188e34d4bde00a8d047f8b85ab
Handole Kim <handolekim@Handoles-MacBook-Pro.local>
2018-10-03 00:08:37 -0400

    WebSocket running on http server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express"),
     bodyParser = require("body-parser"),
     morgan = require("morgan"),
-    Blockchain = require("./blockchain");
+    Blockchain = require("./blockchain"),
+    P2P = require("./p2p");
 
 
 const { getBlockchain, createNewBlock } = Blockchain;
+const { startP2PServer } = P2P;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -22,4 +24,8 @@ app.post("/blocks", (req, res) => {
     res.send(newBlock);
 });
 
-app.listen(PORT, () => console.log(`Handolecoin Server running on ${PORT} ✅`));
+const server = app.listen(PORT, () => 
+    console.log(`Handolecoin Server running on port ${PORT} ✅`)
+);
+
+startP2PServer(server);
