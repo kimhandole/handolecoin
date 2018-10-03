4eeba7cb08eddefb5124b614b48bb266d07c56ef
Handole Kim <handolekim@Handoles-MacBook-Pro.local>
2018-10-03 01:19:28 -0400

    p2p error handling added

--- src/p2p.js
+++ src/p2p.js
@@ -1,5 +1,6 @@
 const WebSockets = require("ws");
 
+// array has peers that connected to my server
 const sockets = [];
 
 const getSockets = () => sockets;
@@ -7,14 +8,31 @@ const getSockets = () => sockets;
 const startP2PServer = server => {
     const wsServer = new WebSockets.Server({ server });
     wsServer.on("connection", ws => {
-        console.log(`Hello Socket!`);
+        initSocketConnection(ws);
     });
     console.log("Handolecoin P2P Server running");
 };
 
 const initSocketConnection = socket => {
     sockets.push(socket);
-}
+    handleSocketError(socket);
+    socket.on("message", (data) => {
+        console.log(data);
+    });
+    setTimeout(() => { 
+        socket.send("welcome");
+    }, 5000);
+};
+
+const handleSocketError = ws => {
+    const closeSocketConnection = ws => {
+        ws.close();
+        sickets.splice(sockets.indexOf(ws), 1);
+    };
+    ws.on("close", () => closeSocketConnection(ws));
+    ws.on("error", () => closeSocketConnection(ws));
+};
+
 
 const connectToPeers = newPeer => {
     const ws = new WebSockets(newPeer);
